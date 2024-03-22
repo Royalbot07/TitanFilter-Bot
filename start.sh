fb16ccf0ddb2e3382aa82234d7bc873de2476465
Royalbot07 <158250775+Royalbot07@users.noreply.github.com>
2024-03-22 20:00:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TitanFilter-Bot
 fi
-cd /DQ-the-file-donor
+cd /TitanFilter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting TitanFilter-Bot...."
 python3 bot.py
